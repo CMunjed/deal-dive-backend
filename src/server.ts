e29f21a424f530f437dacfe2655d84e82b1f5574
CMunjed <cjmunjed@gmail.com>
2025-09-24 18:00:42 -0400

    Revert "Integrated Original Auth0 Configuration for Login / Logout Functionality"

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,9 @@
 
 import app from './app.js';
 
-const PORT = 5001; //process.env.PORT || 5001;
+const PORT = 5000; //process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    console.log(`View in the browser at: http://localhost:${PORT}/`);
-    console.log(`Visit http://localhost:${PORT}/login to log in and http://localhost:${PORT}/logout to log out`);
+    console.log(`View in the browser at http://localhost:${PORT}/`);
 })

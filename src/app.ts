073b8bf2fe447192dbce314f1987c7ebf03a0b58
CMunjed <cjmunjed@gmail.com>
2025-09-26 21:05:20 -0400

    Add routes-controllers-services folder structure, add /dummy endpoint to test supabase connection

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,26 @@
-import express from "express";
-const { default: pkg } = await import("../package.json", {
-  with: {
-    type: "json",
-  },
+import express from 'express';
+import routes from './routes/index.js';
+
+const { default: pkg } = await import('../package.json', {
+    with: {
+        type: "json",
+    },
 });
 
 const app = express();
 
-// Default route + debug message
-app.get("/", (_req, res) => {
+app.use(express.json());
+
+// Debug route
+app.get('/', (_req, res) => {
   res.json({
-    message: "API is running",
-    version: pkg.version,
-    status: "OK",
+      message: 'API is running',
+      version: pkg.version,
+      status: 'OK'
   });
 });
+
+// Main API routes
+app.use("/api/v1", routes);
 
 export default app;

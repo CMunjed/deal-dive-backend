77b25eead6bdfbba5e0527357a4c56f9bcebcf7b
CMunjed <cjmunjed@gmail.com>
2025-10-23 12:52:18 -0400

    Rename votes controller file to match conventions

--- src/routes/votes.routes.ts
+++ src/routes/votes.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addVoteController, removeVoteController, getVotesController } from "../controllers/votes.controllers.js";
+import { addVoteController, removeVoteController, getVotesController } from "../controllers/votes.controller.js";
 
 const router = Router();
 

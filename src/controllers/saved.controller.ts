e21b220fdac93b4fef0a49494d8d4efb45da3941
CMunjed <cjmunjed@gmail.com>
2025-11-06 14:28:29 -0500

    Add route + controller + service file skeletons for saved deals

---
+++ src/controllers/saved.controller.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+import {
+  saveDeal,
+  getSavedDeals,
+  unsaveDeal
+} from "../services/saved.service.js";
+    
+// POST /saved/:dealId
+export async function saveDealController(req: Request, res: Response) {
+  //try {
+    // const userId = req.user.id; // get user.id from auth middleware
+    //const { dealId } = req.params;
+    //const saved = await saveDeal(userId, dealId);
+    //return res.status(201).json(saved);
+  //} catch (error: unknown) {
+  //  return handleError(res, error);
+  //}
+}
+
+// GET /saved
+export async function getSavedDealsController(req: Request, res: Response) {
+  //try {
+  //  const userId = req.user.id;
+  //  const deals = await getSavedDeals(userId);
+  //  return res.status(200).json(deals);
+  //} catch (error: unknown) {
+  //  return handleError(res, error);
+  //}
+}
+
+// DELETE /saved/:dealId
+export async function unsaveDealController(req: Request, res: Response) {
+  //try {
+  //  const userId = req.user.id;
+  //  const { dealId } = req.params;
+  //  await unsaveDeal(userId, dealId);
+  //  return res.status(204).send();
+  //} catch (error: unknown) {
+  //  return handleError(res, error);
+  //}
+}

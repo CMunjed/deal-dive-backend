3e3a1103ec1f6f953239b861388f31169a64b21b
CMunjed <cjmunjed@gmail.com>
2025-11-06 09:33:28 -0500

    Add test for fetching a nonexistent deal

--- tests/deals/deals.routes.test.ts
+++ tests/deals/deals.routes.test.ts
@@ -2,6 +2,23 @@ import request from "supertest";
 import app from "../../src/app.js";
 
 describe("Deal Route Tests", () => {
+    describe("GET /api/v1/deal/:id", () => {
+        /*it("Fetch an existing deal", async () => {
+            
+        });*/
+
+        it("Fetch a nonexistent deal", async () => {
+            const deal_id = "00000000-0000-0000-0000-000000000000";
+            const res = await request(app).get(`/api/v1/deals/${deal_id}`);
+            expect(res.status).toBe(404);
+            expect(res.body).toEqual(
+                expect.objectContaining({
+                    error: expect.any(String),
+                })
+            );
+        });
+    });
+
     describe("GET /api/v1/deals", () => {
         it("Get all deals", async () => {
             const res = await request(app).get('/api/v1/deals');
@@ -9,4 +26,5 @@ describe("Deal Route Tests", () => {
             expect(Array.isArray(res.body)).toBe(true);
         });
     });
+
 });
